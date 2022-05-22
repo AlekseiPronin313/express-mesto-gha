3f2352b541792cff92c6dc44ccf9d6b0ef95e686
billi <isw.pronin@yandex.ru>
2022-05-22 22:59:17 +0300

    test2

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
-// eslint-disable-next-line no-undef
 const { PORT = 3000 } = process.env;
 
-mongoose.connect("mongodb://localhost:27017/mestodb"),
-  {
-    useNewUrlParser: true,
-  };
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 const app = express();
 
@@ -15,17 +13,19 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "628a583ad30831930a2fc319",
+    _id: '628a583ad30831930a2fc319',
   };
 
   next();
 });
 
-app.use("/users", require("./routes/users"));
-app.use("/cards", require("./routes/cards"));
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
-app.use((req, res) => {
-  res.status(404).send({ message: "Страница с таким url не найдена" });
+app.all('*', (req, res) => {
+  res.status(404).send({ message: 'Страница с таким url не найдена' });
 });
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Запуск сервера ${PORT}`);
+});
