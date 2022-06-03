f06f19252ff08a98fc25eb20ee8ae2509d0427c2
billi <isw.pronin@yandex.ru>
2022-06-03 19:47:33 +0300

    err

--- controllers/users.js
+++ controllers/users.js
@@ -111,7 +111,7 @@ const login = (req, res, next) => {
 
   User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'super-strong-secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       res.send(token);
     })
     .catch(() => next(new AuthError('Неверные почта или пароль')));
