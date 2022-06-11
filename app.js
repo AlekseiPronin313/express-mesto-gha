e61aa060a861a835c6acd8f58fd619641155adf0
billi <isw.pronin@yandex.ru>
2022-06-11 15:00:45 +0300

    test

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const {
 const auth = require('./middlewares/auth');
 const handelError = require('./middlewares/handelError');
 const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -19,6 +20,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 const app = express();
 app.use(express.json());
 
+app.use(requestLogger);
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateUser, createUser);
 
@@ -30,6 +32,7 @@ app.all('*', (req, res, next) => {
   next(new NotFoundError('Страница с таким url не найдена'));
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(handelError);
 
